fe3f6b6a9092ade49934186b9e9b6bb5f93015a8
Dima <starchenkodmitrypavlovich@gmail.com>
2023-12-14 13:45:44 +0300

    test 2

--- src/components/MainScene.tsx
+++ src/components/MainScene.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 
 import { Box, CameraControls } from '@react-three/drei'
-import { Vector3 } from 'three'
+import { PerspectiveCamera, Vector3 } from 'three'
 
 import MainModel from './models/MainModel'
 
@@ -16,16 +16,19 @@ function MainScene() {
         <div className='absolute inset-0'>
             <Canvas 
                 className="" //bg-slate-300
-                camera={{
-                    position: [0, 0, 1],
-                    near:0.001,
-                    far:1000
-                }}
+                // camera={{
+                //     position: [0, 0, 1],
+                //     near:0.001,
+                //     far:1000
+                // }}
                 
             >
-                <CameraControls />
+                <CameraControls 
+                    // camera={  new PerspectiveCamera(50,1,0.01,1000) }
+
+                />
                 {/* <pointLight position={[1, 1, 1]} intensity={10} /> */}
-                <ambientLight intensity={3} />
+                <ambientLight intensity={2} />
 
                 <MainModel/>
 
--- src/components/models/MainModel.tsx
+++ src/components/models/MainModel.tsx
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 import { CameraControls, useGLTF } from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
 import React, { useEffect, useRef, useState } from 'react'
@@ -9,16 +9,14 @@ interface Props {}
 
 function MainModel(props: Props) {
     
-    const camera = useThree(state => state.camera)
-    console.log("camera:", camera)
+    const {camera, viewport} = useThree(state => state)
+    console.log("camera:", camera,viewport)
     
     const groupRef = useRef<any>(null)
     const [data,setData] = useState<any>()
 
     useEffect(()=>{
         const loader = new GLTFLoader()
-        // loader.load('/test/Wolf-Blender-2.82a.gltf',(gltf)=>{
-        // loader.load('/scene.glb',(gltf)=>{
         loader.load('/nasa.glb',(gltf)=>{
             console.log("loaded: ",gltf)
             setData(gltf.scene)  
@@ -26,24 +24,22 @@ function MainModel(props: Props) {
     },[])
 
     useFrame(()=>{
-        camera.position.x += 0.1
-
-        // gsap.to(camera.position, {
-        //     x: () => 0,
-        //     y: () => 0,
-        //     duration: 0.5
-        // }) 
+        // camera.position.x += 0.1
+        if(!data)return 
+        data.rotation.y += 0.01
     })
 
-    // const { scene } = useGLTF('/test/Wolf-Blender-2.82a.gltf')
     
+    const { scene } = useGLTF('/nasa.glb')
+
     return (
         <>
             <group ref={groupRef} dispose={null}>
-                {/* <primitive object={scene}></primitive> */}
-                {
-                    !!data && <primitive object={data}></primitive>
-                }
+            {
+                !!data && 
+                <primitive object={data}></primitive>
+            }
+            <primitive object={scene}></primitive>
             </group>
         </>
     )
